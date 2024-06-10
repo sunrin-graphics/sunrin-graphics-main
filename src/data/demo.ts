5da592a9019f492c557b91c5713d1eb5e985f983
jureuk7 <jureuk7@protonmail.com>
2024-06-10 15:12:38 +0900

    feat: add demo,exhibition,presentation data

---
+++ src/data/demo.ts
@@ -0,0 +1,39 @@
+import { Demo } from "@/types/demo";
+
+export const demo: Demo[] = [
+    {
+      year: 2024,
+      title: 'GENESIS',
+      subtitle: '2024 · 5회',
+      description:
+        '선린으로 오는 모든 학생들이 기존의 세계를 파괴하고 미개척지, 세로운 세계를 받아들이는 과정을 조명합니다. 또한 조력자의 위치에서 그들과 함께하며 새로운 변화에 적응할 수 있도록 돕습니다.',
+      link: 'https://genesis.sunrin.graphics'
+    },
+    {
+      year: 2023,
+      title: 'thisPlay!',
+      subtitle: '2023 · 4회',
+      description:
+        '상상을 현실로 만드는 힘을 가진 우리의 놀이터에서 새롭게 꿈을 펼칠 여러분을 위해 작은 선물을 준비했어요!',
+      link: 'https://thisplay.sunrin.graphics'
+    },
+    {
+      year: 2022,
+      title: '飛 : 날 비',
+      subtitle: '2022 · 3회',
+      description: '비상이라는 단어를 정의할 우리'
+    },
+    {
+      year: 2021,
+      title: '시작',
+      subtitle: '2021 · 2회',
+      description: '시작 : 비로소 빛나다'
+    },
+    {
+      year: 2020,
+      title: 'st, ART',
+      subtitle: '2020 · 1회',
+      description: '세상이라는 종이를 예술이라는 색연필로 칠하는',
+    }
+  ]
+  
---
+++ src/data/presentation.ts
@@ -0,0 +1,12 @@
+import { Demo } from "@/types/demo";
+
+export const presentation: Demo[] = [
+    {
+      year: 2023,
+      title: 'Somnium',
+      subtitle: '2023 · 1회',
+      description:
+        '우리는 새로운 시작과 끝, 경계 사이에 서있습니다 이곳에서는 우리는 한 해동안 꿈꿔왔던 모든것이 제각각 색을 내며 공생합니다 지나치게 비현실적이고 아름다운 이 곳, 우리의 유토피아에 당신을 초대합니다'
+    }
+  ]
+  
