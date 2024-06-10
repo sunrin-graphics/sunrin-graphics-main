b18bb47425b2f2af21b6639f4c7d2170ab3c2d3c
jureuk7 <jureuk7@protonmail.com>
2024-06-10 15:10:39 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+    ],
+  },
+
+  plugins: [react(), tsconfigPaths(), svgr()],
+});
